9c17dfdf64ae6fb7766514f0ce4c60a8fd3390cd
SREERAJ V P <sreerajsree007web@gmail.com>
2026-03-19 11:31:53 +0530

    otpprob

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,4 +1,29 @@
 
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import nodemailer from "nodemailer";
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS
+//   }
+// });
+
+// const sendEmail = async ({ to, subject, html }) => {
+  
+//   await transporter.sendMail({
+//     from: `"Your App Name" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject,
+//     html
+//   });
+// };
+
+// export default sendEmail;
+
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -13,13 +38,24 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async ({ to, subject, html }) => {
-  await transporter.sendMail({
-    from: `"Your App Name" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html
-  });
+  try {
+    // 🔥 ADD LOGS HERE
+    console.log("EMAIL_USER:", process.env.EMAIL_USER);
+    console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
+
+    const info = await transporter.sendMail({
+      from: `"Your App Name" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html
+    });
+
+    console.log("✅ EMAIL SENT:", info.response);
+
+  } catch (error) {
+    console.error("❌ EMAIL ERROR:", error);
+    throw error;
+  }
 };
 
 export default sendEmail;
-
